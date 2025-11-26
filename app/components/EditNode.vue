b54b5e654d1bf0bf1d36dc033457e518eded208e
Jakob Olason <jakobolason@proton.me>
2025-11-26 19:37:13 +0100

    Uses standard nuxt ui template

---
+++ app/components/EditNode.vue
@@ -0,0 +1,9 @@
+<template>
+  <USlideover>
+    <UButton label="Open" color="neutral" variant="subtle" />
+
+    <template #content>
+      <Placeholder class="h-full m-4" />
+    </template>
+  </USlideover>
+</template>
---
+++ app/pages/tree.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="tree-wrapper">
+    <div class="toolbar">
+      <h1>Family Tree</h1>
+    </div>
+
+    <FamilyTree />
+    <EditNode />
+  </div>
+</template>
